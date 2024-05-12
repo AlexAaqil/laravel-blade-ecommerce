874796f4ab663b4718d63b5257a21651347043e5
alexaaqil <alexaaqil.se@gmail.com>
2024-05-12 11:29:44 +0300

    First commit after adding files from the laravel template.

---
+++ resources/views/index.blade.php
@@ -0,0 +1,7 @@
+<x-general-layout class="HomePage">
+    <section class="Hero">
+        <div class="container">
+            <h1>Hero Section</h1>
+        </div>
+    </section>
+</x-general-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,36 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\GeneralPagesController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\CommentController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [GeneralPagesController::class, 'home'])->name('home');
+Route::get('/about', [GeneralPagesController::class, 'about'])->name('about');
+Route::get('/services', [GeneralPagesController::class, 'services'])->name('services');
+Route::get('/contact', [GeneralPagesController::class, 'contact'])->name('contact');
+Route::post('/contact', [CommentController::class, 'store'])->name('comments.store');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/home', [DashboardController::class, 'index'])->name('dashboard.index');
+    Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
+    
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';
+
+Route::middleware(['auth', 'verified', 'admin'])
+->prefix('admin')
+->group(function() {
+    Route::get('/dashboard', [DashboardController::class, 'admin_dashboard'])->name('admin.dashboard');
+
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+
+    Route::resource('comments', CommentController::class)->only('index', 'edit', 'update', 'destroy');
+});
