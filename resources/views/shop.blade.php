afc719ce5257836cdb3e823e6208d3e7c35678a3
alexaaqil <alexaaqil.se@gmail.com>
2024-06-24 16:54:21 +0300

    shop: modified the structure and styles of the shop page.

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -1,23 +1,27 @@
 <x-general-layout class="Shop">
-    <div class="container search">
-        <input type="text" name="search" id="search" placeholder="Search Products">
-    </div>
+    <section class="Header">
+        <div class="container">
+            <input type="text" name="search" id="search" placeholder="Search Products">
+        </div>
+    </section>
 
-    <div class="container categories">
-        <a href="#">all</a>
-        <a href="#">electronics</a>
-        <a href="#">shoes</a>
-        <a href="#">computers</a>
-        <a href="#">clothes</a>
-        <a href="#">TVs</a>
-        <a href="#">cameras</a>
-        <a href="#">phone</a>
-        <a href="#">accessories</a>
-        <a href="#">bags</a>
-        <a href="#">chargers</a>
-        <a href="#">laptops</a>
-        <a href="#">printers</a>
-    </div>
+    <section class="Categories">
+        <div class="container">
+            <a href="#">all</a>
+            <a href="#">electronics</a>
+            <a href="#">shoes</a>
+            <a href="#">computers</a>
+            <a href="#">clothes</a>
+            <a href="#">TVs</a>
+            <a href="#">cameras</a>
+            <a href="#">phone</a>
+            <a href="#">accessories</a>
+            <a href="#">bags</a>
+            <a href="#">chargers</a>
+            <a href="#">laptops</a>
+            <a href="#">printers</a>
+        </div>
+    </section>
 
     <section class="Products">
         <div class="container">
